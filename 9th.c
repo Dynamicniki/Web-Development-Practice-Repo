4e42eabeff6dd54c5560e0ce14b2117b20b5d4ba
Dynamicniki <nikhileshitm105@gmail.com>
2025-01-11 21:55:33 +0530

    Add files via upload

---
+++ 9th.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int x = 10, y = 20;
+    int result = add(x, y);
+    printf("Sum of %d and %d is %d\n", x, y, result);
+    return 0;
+}
